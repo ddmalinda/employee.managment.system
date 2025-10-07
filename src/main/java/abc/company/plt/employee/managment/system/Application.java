ad4f2877862abb7b7e8892fc489ec95e558c3fa7
Dasith Dilina Malinda <dasithdilinamalinda@gmail.com>
2025-10-07 21:15:19 +0530

    initial commit

---
+++ src/main/java/abc/company/plt/employee/managment/system/Application.java
@@ -0,0 +1,13 @@
+package abc.company.plt.employee.managment.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/abc/company/plt/employee/managment/system/ApplicationTests.java
@@ -0,0 +1,13 @@
+package abc.company.plt.employee.managment.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
